9829305cb5733c969a469c899679bb41725530ea
AdamRayBraun <adambraun@gmail.com>
2017-11-27 19:18:02 +0000

    Looping up through PWM intensity

--- outputs/GPIO_vibration-disc/GPIO_vibration-disc.ino
+++ outputs/GPIO_vibration-disc/GPIO_vibration-disc.ino
@@ -1,14 +1,18 @@
 const int motorPin = 3;
 
-void setup()
-{
-pinMode(motorPin, OUTPUT);
+void setup() {
+    Serial.begin(9600);
+    pinMode(motorPin, OUTPUT);
 }
 
-void loop()
-{
-analogWrite(motorPin, 153);
-delay(1000);
-analogWrite(motorPin, 0);
-delay(2000);
+void loop() {
+
+    for (int x = 0; x < 154; x ++) {
+        analogWrite(motorPin, 153);
+        Serial.print("Pulse val: ");
+        Serial.println(x);
+        delay(10);
+
+    analogWrite(motorPin, 0);
+    delay(3000);
 }
