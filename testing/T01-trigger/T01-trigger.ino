d1b04072e1e630879bf002c41290f1701cd1f6d1
AdamRayBraun <adambraun@gmail.com>
2018-05-16 14:44:30 +0100

    Testing triggering sounds through circuit

---
+++ testing/T01-trigger/T01-trigger.ino
@@ -0,0 +1,10 @@
+int SB_BONE[] = {29,31,33,35,37,39,41,43,45,47,49};
+int SB_BONE_RST = 27;
+
+void setup() {
+  pinMode(31, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(31, LOW);
+}
--- testing/soundboard-singleTrigger/soundboard-singleTrigger.ino
+++ testing/soundboard-singleTrigger/soundboard-singleTrigger.ino
@@ -1,29 +1,51 @@
-int SB_SUB_1 = 40;
+int SB_SUB[] = {42,40,38,36,34,32,30,28,26,24,22};
 int SB_SUB_RST = 44;
-int SB_BONE_1 = 31;
+
+int SB_BONE[] = {29,31,33,35,37,39,41,43,45,47,49};
 int SB_BONE_RST = 27;
 
 void setup() {
-  pinMode(SB_SUB_1, OUTPUT);
   pinMode(SB_SUB_RST, OUTPUT);
-  pinMode(SB_BONE_1, OUTPUT);
   pinMode(SB_BONE_RST, OUTPUT);
+
+  for (int x=0; x<11; x++) {
+    pinMode(SB_BONE[x], OUTPUT);
+  }
 }
 
 void loop() {
   // Reset both soundboards
   digitalWrite(SB_SUB_RST, LOW);
-  digitalWrite(SB_SUB_1, HIGH);
   digitalWrite(SB_BONE_RST, LOW);
-  digitalWrite(SB_BONE_1, HIGH);
+
+  for (int x=0; x<11; x++) {
+    digitalWrite(SB_BONE[x], HIGH);
+  }
+
+  for (int x=0; x<11; x++) {
+    digitalWrite(SB_SUB[x], HIGH);
+  }
 
   delay(5000); //wait 5 secs
 
   // Trigger T01 on both soundboards
   digitalWrite(SB_SUB_RST, HIGH);
-  digitalWrite(SB_SUB_1, LOW);
-  digitalWrite(SB_BONE_RST, HIGH);
-  digitalWrite(SB_BONE_1, LOW);
+  digitalWrite(SB_SUB_RST, HIGH);
+
+  for (int x=0; x<11; x++) {
+    if (x == 1) {
+      digitalWrite(SB_SUB[x], LOW);
+    } else
+    digitalWrite(SB_SUB[x], HIGH);
+  }
+
+  for (int x=0; x<11; x++) {
+    if (x == 1) {
+      digitalWrite(SB_BONE[x], LOW);
+    } else {
+      digitalWrite(SB_BONE[x], HIGH);
+    }
+  }
 
   delay(10000); //wait 10 secs
 }
